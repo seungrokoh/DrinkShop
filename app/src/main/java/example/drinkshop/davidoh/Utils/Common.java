2c2befcb2281921361d2b92603688fe2bb6d4185
seungrokoh <devdavidoh@gmail.com>
2019-04-09 20:26:39 +0900

    fix : .gitignore

--- app/src/main/java/example/drinkshop/davidoh/Utils/Common.java
+++ app/src/main/java/example/drinkshop/davidoh/Utils/Common.java
@@ -7,13 +7,12 @@ import example.drinkshop.davidoh.model.User;
 
 public class Common {
     //In Emulator, localhost = 10.2.2.2
-    private static final String BASE_URL = "http://plplim.ipdisk.co.kr:8000/drinkshop/";
-
+    //private static final String BASE_URL = ;
     public static User currentUser = null;
 
     public static Category currentCategory = null;
 
     public static IDrinkShopAPI getAPI(){
-        return RetrofitClient.getClient(BASE_URL).create(IDrinkShopAPI.class);
+        return RetrofitClient.getClient(RemoteConfig.BASE_URL).create(IDrinkShopAPI.class);
     }
 }
--- app/src/main/java/example/drinkshop/davidoh/Utils/RemoteConfig.java
+++ app/src/main/java/example/drinkshop/davidoh/Utils/RemoteConfig.java
@@ -1,4 +1,5 @@
 package example.drinkshop.davidoh.Utils;
 
 public class RemoteConfig {
+    static final String BASE_URL = "http://plplim.ipdisk.co.kr:8000/drinkshop/";
 }
